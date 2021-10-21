35b01b9bdb24162450e0c4a0a8ac72f8173e59f4
lehoa08121998@gmail.com <lehoa08121998@gmail.com>
2021-10-22 00:59:35 +0700

    Add routing and navigating programatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ListItemComponent } from './list/list-item/list-item.component';
+import { ListComponent } from './list/list.component';
 import { UserRootService } from './services/user-root.services';
 import { ItemsService } from './services/items.services';
 import { UpdateItemsService } from './services/update-item.service';
@@ -5,13 +7,21 @@ import { UpdateItemsService } from './services/update-item.service';
 
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListModule } from './list/list.module';
 import { TableComponent } from './table/table.component';
 import { FindItemsService } from './services/find-item.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRouter: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'user', component: ListComponent },
+  { path: 'itemList', component: HeaderComponent },
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +32,7 @@ import { FindItemsService } from './services/find-item.service';
     BrowserModule,
     AppRoutingModule,
     ListModule,
+    RouterModule.forRoot(appRouter)
 
   ],
   providers: [FindItemsService, ItemsService, UserRootService, UpdateItemsService],
